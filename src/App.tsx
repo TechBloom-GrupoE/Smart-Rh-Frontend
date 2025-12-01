daa0174707ca5ffe23889a0ccc1dc086441b7e3a
Priscila Mrozinski <priscila_alins@hotmail.com>
2025-12-01 10:15:15 -0300

    Atualização Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Navbar from "./componentes/navbar/Navbar"
 import Home from "./pages/home/Home"
 
 function App() {
 	return (
-		<>
-	
-		<Navbar />
-		<Home />
+		<BrowserRouter>
+			<Navbar menuState={"closed"} onMenuToggle={function (): void {
+				throw new Error("Function not implemented.")
+			} } onMenuClose={function (): void {
+				throw new Error("Function not implemented.")
+			} } />
+			<div>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					{/* <Route path="/colaboradores" element={<ListarColaboradores />} />
+					<Route path="/cadastrarcolaborador/:id" element={<FormColaborador />} />
+					<Route path="/editarcolaborador/:id" element={<FormColaborador />} />
+					<Route path="/deletarcolaborador/:id" element={<DeletarColaborador />} /> */}
+				</Routes>
+			</div>
+		</BrowserRouter>
+
+		// <>
+
+		// <Navbar />
+		// <Home />
 
 
-		</>
+		// </>
 	)
 }
 
 export default App
+
--- src/componentes/navbar/Navbar.tsx
+++ src/componentes/navbar/Navbar.tsx
@@ -1,18 +1,78 @@
-function Navbar() {
+import { SignOutIcon, UserCircleIcon } from "@phosphor-icons/react";
+import { useRef } from "react";
+import { Link } from "react-router-dom";
+
+
+type MenuState = 'closed' | 'open';
+
+interface NavbarProps {
+  menuState: MenuState;
+  onMenuToggle: () => void;
+  onMenuClose: () => void;
+};
+
+
+function Navbar({ menuState, onMenuToggle, onMenuClose }: Readonly<NavbarProps>) {
+
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  const handleMenuToggle = (): void => {
+    onMenuToggle();
+  };
+
+  // Handler para fechar o menu mobile, ao clicar no botão X
+  const handleMenuClose = (): void => {
+    onMenuClose();
+  };
+
+
+
+
+
   return (
     <>
-      <div className='w-full flex justify-center py-4 bg-indigo-900 text-white'>
-        <div className="container flex justify-between text-lg mx-8">
-          Logo
-          <div className='flex gap-4'>
-            Colaboradores
-            Departamentos
-            Sobre nós
-            Perfil
-            Sair
+      <header className="w-full bg-white shadow">
+        <div className="w-full flex justify-between items-center py-3 px-12">
+
+          {/* INÍCIO - Div do logo e Título SmartRH */}
+
+          <div className="flex items-center gap-3 w-1/2 py-6">
+            <Link to='/home'>
+              <img
+                src="https://ik.imagekit.io/p9rdkccyy/smartrh_logo_horizontal.png.png"
+                alt="Logo"
+                className="w-70" // ajusta o tamanho; pode alterar
+              />
+            </Link>
+
+           {/* Logo escrit embaixo:  className="w-50 md:w-60" */}
+
+
           </div>
+
+          {/* FIM - Div do logo e Título SmartRH */}
+
+
+          {/* INÍCIO - MENU*/}
+          <nav className="md:flex items-center gap-6 text-[15px] font-semibold">
+            {/* <div className="relative flex items-center justify-center w-2/5 text-black max-md:hidden">
+              <SearchForm />
+            </div> */}
+
+            {/* Menu de navegação desktop/tablet */}
+            {/* <div className='items-center hidden gap-4 py-4 md:flex'> */}
+              <Link to='/colaboradores' className='text-2xl px-4 text-orange-500 hover:opacity-50'>Colaboradores</Link>
+              <Link to='/departamentos' className='text-2xl px-4 text-orange-500 hover:opacity-50'>Departamentos</Link>
+              <Link to='/calcularsalario' className='text-2xl px-4 text-orange-500 hover:opacity-50'>Calcular Salário</Link>
+              <Link to='/sobrenos' className='text-2xl px-4 text-black hover:opacity-50'>Sobre nós</Link>
+
+              <Link to='/perfil'><UserCircleIcon size={50} weight='fill'/></Link>
+              <Link to='/sair'><SignOutIcon size={45} weight='bold'/></Link>
+              
+       
+          </nav>
         </div>
-      </div>
+      </header>
     </>
   )
 }
