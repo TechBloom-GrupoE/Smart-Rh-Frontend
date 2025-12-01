37ca28cab8653000ca3027f297f48d7822d50c4a
andresssafunes <andressafunes92@gmail.com>
2025-12-01 12:26:40 -0300

    :zap: atualização

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,34 @@ import ListarDepartamento from "./componentes/departamento/listardepartamento/Li
 import FormDepartamento from "./componentes/departamento/formdepartamento/FormDepartamento"
 import DeletarDepartamento from "./componentes/departamento/deletardepartamento/DeletarDepartamento"
 import Navbar from "./componentes/navbar/Navbar"
-import Home from "./pages/home/Home"
+import ListarColaboradores from "./coaborador/listarcolaboradores/ListarColaboradores"
+import FormColaborador from "./coaborador/formcolaborador/FormColaborador"
+import DeletarColaborador from "./coaborador/deletarcolaborador/DeletarColaborador"
 
 function App() {
 	return (
 		<>
-			<BrowserRouter>
-				<Navbar />
-				<Home />
-				<Footer />
-			</BrowserRouter>
+		<BrowserRouter>
+        <Navbar menuState={"closed"} onMenuToggle={function (): void {
+          throw new Error("Function not implemented.")
+        }} onMenuClose={function (): void {
+          throw new Error("Function not implemented.")
+        }} />
+                <div>
+                <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/departamentos" element={<ListarDepartamento />}/>
+				<Route path="/caddepartamento" element={<FormDepartamento />} />
+              	<Route path="/editardepartamento/:id" element={<FormDepartamento />} />
+              	<Route path="/deletardepartamento/:id" element={<DeletarDepartamento />} />
+				<Route path="/colaboradores" element={<ListarColaboradores />}/>
+				<Route path="/colaboradores" element={<FormColaborador />}/>
+				<Route path="/editarcolaborador/:id" element={<FormColaborador />}/>
+                <Route path="/deletarcolaborador/:id" element={<DeletarColaborador/>}/>
+                </Routes>
+                </div>
+                <Footer />
+            </BrowserRouter>
 		</>
 	)
 }
